bf979a97a4f267b9a596af0812bfe15744e3386c
Christopher Dorst <chris@dorst.email>
2018-01-02 00:53:50 -0600

    Add Users helper

---
+++ Metaproject.Users.CDorst.GitCommit/Users.cs
@@ -0,0 +1,12 @@
+﻿using DevOps.VersionControl.Structures.GitCommitUser;
+
+namespace Metaproject.Users.CDorst.GitCommit
+{
+    public static class Users
+    {
+        private const string Email = "chris@dorst.email";
+        private const string Name = "Christopher Dorst";
+
+        public static UserInfo CDorst = new UserInfo(Email, Name);
+    }
+}
